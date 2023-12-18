e51b6bd10a9e67fff083e50aa6f4f1cba6bea114
Aayush Subedi <subediaayush@gmail.com>
2023-12-18 19:41:10 +0000

    Enable search query and debouncer implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { SubTitle, Title } from './components/styled/typeface';
-import { Padding, Row } from './components/styled/layout';
+import { StatusChip, SubTitle, TableItem, Title, ValueItem } from './components/styled/typeface';
+import { Body, Padding, Row } from './components/styled/layout';
 import { Plain } from './components/styled/badges';
-import { Table } from './components/data/Table';
+import { RemoteTable } from './components/data/Table';
+import { useDebouncer } from './hooks/useDebouncer';
+import { Input } from './components/styled/input';
 
 function App() {
+
+  const defaultPayoutUrl = 'https://theseus-staging.lithium.ventures/api/v1/analytics/tech-test/payouts'
+  const headers: {} = { 'Date & Time': 'dateAndTime', 'User': 'username', 'Status': 'status', 'Value': 'value' }
+
+  const [payoutApiUrl, setPayoutApiUrl] = useState(`${defaultPayoutUrl}`);
+
+  const debounce = useDebouncer(500, (query: string) => {
+    if (query) {
+      setPayoutApiUrl(() => `${defaultPayoutUrl}?query=${query}`)
+    } else {
+      setPayoutApiUrl(() => defaultPayoutUrl)
+    }
+  });
+
+  const getCellValue = useCallback((value: any, key: string) => {
+    switch (key) {
+      case 'status':
+        return <StatusChip status={value}><TableItem>{value}</TableItem></StatusChip>
+      case 'dateAndTime':
+        const formattedDate = new Date(value).toLocaleString("en-US", { weekday: "short", month: "short", day: "numeric" });
+        return <TableItem>{formattedDate}</TableItem>
+      case 'username':
+        return <TableItem>{value}</TableItem>
+      case 'value':
+        return <ValueItem>{value}</ValueItem>
+    }
+
+    return <></>
+  }, []);
+
   return (
-    <div>
+    <Body>
       <Title>
-        Title
+        Payouts
       </Title>
       <Padding $left="1rem" $right="1rem">
         <SubTitle>
@@ -19,8 +51,16 @@ function App() {
             Payout History
           </Row>
         </SubTitle>
+        <Input onChange={(e) => debounce(e.target.value)} placeholder='Search' type='search' />
+        <RemoteTable
+          enablePagination
+          headers={Object.keys(headers)}
+          headersMapping={headers}
+          url={payoutApiUrl}
+          cell={getCellValue}
+        />
       </Padding>
-    </div>
+    </Body>
   );
 }
 
